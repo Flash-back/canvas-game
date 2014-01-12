c43b959d6b69411446f2a7debf7a9f09c0ce08b0
Alfonso <alfonso.cama@hotmail.com>
2014-01-12 10:25:01 +0100

    Archivos subidos. Añadido login y sistema de comunicacion de movimiento, asi como otras estructuras basicas de comunicacion entre usuarios.

---
+++ app.coffee
@@ -0,0 +1,49 @@
+###
+Module dependencies.
+###
+
+express = require("express")
+routes = require('./routes')
+user = require('./routes/user')
+http = require("http")
+path = require("path")
+app = express()
+server =  require("http").createServer(app)
+io = require('socket.io').listen(server)
+
+
+server.listen(80);
+
+app.configure ->
+    app.set "port", process.env.PORT or 3000
+    app.set "views", __dirname + "/views"
+    app.set "view engine", "jade"
+    app.use express.favicon()
+    app.use express.logger("dev")
+    app.use express.bodyParser()
+    app.use express.methodOverride()
+    app.use app.router
+    app.use require("stylus").middleware(__dirname + "/public")
+    app.use express.static(path.join(__dirname, "public"))
+
+app.configure "development", ->
+    app.use express.errorHandler()
+
+io.sockets.on 'connection', () ->
+    console.log "aaaaaaaaaaaaaaaa"
+
+app.get "/", (req,res) ->
+    routes.index
+    
+app.get "/users", user.list
+
+http.createServer(app).listen app.get("port"), ->
+    console.log "Express servidor listening on port " + app.get("port")
+
+
+
+    #socket.on 'my other event', (data)->
+     #   console.log data
+
+
+    
---
+++ app.js
@@ -0,0 +1,80 @@
+///////////////BASIC SETTING////////////////////////////////////////////////////
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var path = require('path');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var usuarios = new Array(); //Todos los usuarios conectados del juego
+var usuario = new Array(); //Usuario cliente
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+//////////////////////////////////VARIABLES////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+app.get('/', routes.index);
+
+//app.get('/users', user.list);
+
+
+io.sockets.on('connection', function (socket) {
+	socket.on('login', function(data,callback){
+		callback(true);
+		console.log("Logueado "+data);
+		usuario = {'id':usuarios.length,'nombre':data,'vida':100,'x':10,'y':10};
+		usuarios.push(usuario);
+		socket.usuario = usuario;
+		io.sockets.emit('usuarios', usuarios);
+		socket.broadcast.emit('anuncio', {
+	        nick: 'system',
+	        message: data + ' conectado' 
+      	});
+	});
+
+	socket.on('movimiento', function (data){
+		console.log(usuarios.indexOf(socket.usuario));
+		if (data.eje == 'x'){
+			usuarios[usuarios.indexOf(socket.usuario)].x += data.cantidad;
+			socket.usuario.x += data.cantidad;
+		};
+		io.sockets.emit('movimiento', {'id':socket.usuario.id, 'eje':'x','cantidad':10});
+	});
+
+	socket.on('disconnect', function () {
+	    if (!socket.usuario) return;
+	    if (usuarios.indexOf(socket.usuario) > -1) {
+	      usuarios.splice(usuarios.indexOf(socket.usuario), 1);
+	    }
+	    socket.broadcast.emit('anuncio', {
+	      nick: 'system',
+	      message: socket.usuario['nombre'] + ' disconnected' 
+	    });
+	    io.sockets.emit('usuarios', usuarios);
+ 	});
+});
+
+server.listen(app.get('port'), function()
+{
+	console.log('Express server listening on port ' + app.get('port'));
+});
